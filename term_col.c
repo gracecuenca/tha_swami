c2c230abd75eefbc337b7eee9cf09e9057aabd94
Your Name <you@example.com>
2018-01-09 15:26:42 -0500

    colors!

--- term_col.c
+++ term_col.c
@@ -22,30 +22,6 @@ void clearscreen(){
   printf(CLEAR);
 }
 
-int print_change(char * base) {
-  int row, col, i;
-  row = rows();
-  col = cols();
-  //clearscreen(row);
-  printf("col: %d\n", col);
-  printf("a");
-  char change_color[1000];
-  printf("a");
-  char one_row[1000];
-
-  for (i = 0; i < col; i ++) {
-    strcat(one_row, "B");
-    printf("b");
-  }
-  strcat(one_row, "\n");
-
-  for (i = 0; i < row; i ++) {
-    strcat(change_color, one_row);
-  }
-  printf(RED "%s" RESET, change_color);
-  return 0;
-}
-
 char * change_color(char * color){
   clearscreen();
   int i;
@@ -58,6 +34,7 @@ char * change_color(char * color){
   }
   printf("%s", color);
   printf("%s", screen);
+  printf("\n");
   printf(RESET);
   return screen;
 }
@@ -67,17 +44,17 @@ int main() {
   //print_change(RED);
   //print_change(RESET);
   color = change_color(GREY);
-  sleep(3);
+  sleep(1);
   color = change_color(RED);
-  sleep(3);
+  sleep(1);
   color = change_color(GREEN);
-  sleep(3);
+  sleep(1);
   color = change_color(YELLOW);
-  sleep(3);
+  sleep(1);
   color = change_color(BLUE);
-  sleep(3);
+  sleep(1);
   color = change_color(CYAN);
-  sleep(3);
+  sleep(1);
   color = change_color(MAGENTA);
   free(color);
   return 0;
