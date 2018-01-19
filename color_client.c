32abf7972f6887e80354ad26d66f24f4c0b32087
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2018-01-19 16:45:35 -0500

    colors

--- color_client.c
+++ color_client.c
@@ -4,6 +4,7 @@ int main(int argc, char **argv) {
 
   int server_socket;
   char buffer[BUFFER_SIZE];
+  char buffer2[BUFFER_SIZE];
   char * color;
   int counter = 0; //temp for now just to keep track
 
@@ -12,7 +13,20 @@ int main(int argc, char **argv) {
   else
     server_socket = client_setup( TEST_IP );
 
+  write(server_socket, "SETUP", sizeof("SETUP"));
   while (1) {
+    strncpy(buffer2, buffer, BUFFER_SIZE);
+    read(server_socket, buffer, sizeof(buffer));
+    //printf("READ");
+    color = change_color(buffer);
+    //printf("%s", color);
+    if (getchar() == '\n') {
+      write(server_socket, buffer2, sizeof(buffer2));
+    }
+    else {
+      write(server_socket, "TEST", sizeof("TEST"));
+    }
+    /***
     write(server_socket, buffer, sizeof(buffer));
     read(server_socket, buffer, sizeof(buffer));
     color = change_color(buffer);
@@ -30,6 +44,7 @@ int main(int argc, char **argv) {
 
     //free(color);
     //printf("received: [%s]\n", buffer);
+    ***/
   }
   //free(color);
 }
--- forking_server.c
+++ forking_server.c
@@ -1,24 +1,26 @@
 #include "headers.h"
+#include <time.h>
 
 void process(char *s);
 void subserver(int from_client);
 
-int pids [5] = malloc(5, sizeof(int * ));
+static int pids[5];
 int i = 0;
+int can_change = 0;
+int started = 0;
 
 int main() {
-
   int listen_socket;
   int f;
   listen_socket = server_setup();
-
+  for (int a = 0; a < 5; a ++) {
+    pids[a] = 0;
+  }
   while (1) {
 
     int client_socket = server_connect(listen_socket);
     f = fork();
     if (f == 0) {
-      pids[i] = getpid();
-      i ++;
       subserver(client_socket);
     }
     else {
@@ -28,22 +30,45 @@ int main() {
 }
 
 int getRandPID() {
-
+  srand(time(NULL));
+  int r = rand() % 5;
+  if (!pids[r]) {
+    getRandPID();
+  }
+  printf("PID: %d\n", pids[r]);
+  return pids[r];
 }
 
 void subserver(int client_socket) {
   char buffer[BUFFER_SIZE];
-
+  pids[i] = getpid();
+  printf("pids[%d]: %d\n", i, getpid());
+  i ++;
+  
   while (read(client_socket, buffer, sizeof(buffer))) {
-    //printf("[subserver %d] received: [%s]\n", getpid(), buffer);
+    printf("[subserver %d] received: [%s]\n", getpid(), buffer);
     //initial setup to turn all clients red
     write(client_socket, RED, sizeof(char *));
 
+    
     //game start fxn
-
+    if (!started && getpid() == getRandPID()) {
+      write(client_socket, CYAN, sizeof(char *));
+      started = 1;
+    }
+    
+    //write(client_socket, CYAN, sizeof(char *));
+    
     //if read = blue
-    //getrandpid
-    //if getpid = getrandpid write blue
+    if (!strcmp(CYAN, buffer)) {
+      //getrandpid
+      can_change = 1;
+    }
+    if (can_change && getpid() == getRandPID()) {
+      //if getpid = getrandpid write blue
+      write(client_socket, CYAN, sizeof(char *));
+      can_change = 0;
+    }
     
   }//end read loop
   close(client_socket);
