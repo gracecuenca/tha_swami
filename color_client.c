f01277a05d3fe7f5df2277534001199d9acaa801
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2018-01-23 15:28:12 -0500

    nothing works

--- color_client.c
+++ color_client.c
@@ -3,11 +3,13 @@
 int main(int argc, char **argv) {
 
   int server_socket;
-  char buffer[BUFFER_SIZE];
-  char buffer2[BUFFER_SIZE];
+  char buffer[BUFFER_SIZE]; //color read from server
+  char buffer2[BUFFER_SIZE]; //previous color
+  char buffer3[BUFFER_SIZE]; //stdin
   char * color;
   int counter = 0; //temp for now just to keep track
-
+  fd_set read_fds;
+  
   if (argc == 2)
     server_socket = client_setup( argv[1]);
   else
@@ -15,36 +17,39 @@ int main(int argc, char **argv) {
 
   write(server_socket, "SETUP", sizeof("SETUP"));
   while (1) {
-    strncpy(buffer2, buffer, BUFFER_SIZE);
-    read(server_socket, buffer, sizeof(buffer));
-    //printf("READ");
-    color = change_color(buffer);
-    //printf("%s", color);
-    if (getchar() == '\n') {
-      write(server_socket, buffer2, sizeof(buffer2));
-    }
-    else {
-      write(server_socket, "TEST", sizeof("TEST"));
-    }
-    /***
-    write(server_socket, buffer, sizeof(buffer));
-    read(server_socket, buffer, sizeof(buffer));
-    color = change_color(buffer);
-    if (color) {
-      write(server_socket, "CHANGED COLOR", 256);
-    }
-    else {
-      write(server_socket, "NOT CHANGED COLOR", 256);
+    fflush(stdout);
+    FD_ZERO(&read_fds);
+    FD_SET(STDIN_FILENO, &read_fds);
+    FD_SET(server_socket, &read_fds);
+
+    select(server_socket + 1, &read_fds, NULL, NULL, NULL);
+
+    if (FD_ISSET(STDIN_FILENO, &read_fds)) {
+      fgets(buffer3, sizeof(buffer3), stdin);
+      if (!strcmp(buffer3, "\n")) {
+	write(server_socket, buffer, sizeof(buffer));
+      }
+      //write(server_socket, "NO", sizeof("NO"));
     }
 
-    printf("%d\n", counter);
-    //awaiting user to hit enter, incidicating that it's reached the computer to wack
-    getchar();
-    counter++;
-
-    //free(color);
-    //printf("received: [%s]\n", buffer);
-    ***/
+    if (FD_ISSET(server_socket, &read_fds)) {
+      
+      
+      if (read(server_socket, buffer, sizeof(buffer)) == -1) {
+	printf("%s\n", strerror(errno));
+	exit(0);
+      }
+      //strncpy(buffer2, buffer, BUFFER_SIZE);
+      //printf("READ");
+      //printf("%s", buffer);
+      color = change_color(buffer);
+      // printf("COLOR %s", color);
+      //write(server_socket, "", sizeof(char *));
+      //write(server_socket, buffer2, sizeof(buffer2));
+      if (fflush(stdout) != 0) {
+	printf("%s\n", strerror(errno));
+	exit(0);
+      }
+    }
   }
-  //free(color);
 }
--- forking_server.c
+++ forking_server.c
@@ -1,5 +1,7 @@
 #include "headers.h"
 
+int pid_pos = 2;
+
 void process(char *s);
 //todo: separate subservers into three functionalities
 //void color_subserver(int from_client);
@@ -8,10 +10,12 @@ void process(char *s);
 void subserver(int from_client);
 
 int main() {
-  int listen_socket;
+  int listen_socket, client_socket;
   int f;
   int a;
-
+  char * buffer[BUFFER_SIZE];
+  fd_set read_fds;
+  int subserver_count = 0;
   //shared memory initizlizations
   int shmid = shmget(KEY, 7*sizeof(int), IPC_CREAT | 0666);
   if(shmid < 0){
@@ -23,18 +27,27 @@ int main() {
   //the rest are actual pids
   int* pids = shmat(shmid, 0, 0);
   if((int)pids < 0){
+    printf("%s\n", strerror(errno));
     printf("error in shmat\n");
   }
   for (a = 0; a < 7; a ++) {
     pids[a] = 0;
-    printf("%d\n", pids[a]);
+    printf("pids[%d] : %d\n", a, pids[a]);
   }
-
   listen_socket = server_setup();
 
   while (1) {
 
-    int client_socket = server_connect(listen_socket);
+    //if listen_socket triggered select
+      client_socket = server_connect(listen_socket);
+      
+      f = fork();
+      if (f == 0)
+	subserver(client_socket);
+      else {
+	close(client_socket);
+      }
+    }//end listen_socket select
     f = fork();
     if (f == 0) {
       subserver(client_socket);
@@ -42,6 +55,7 @@ int main() {
     else {
       close(client_socket);
     }
+    pid_pos++;
   }
 }
 
@@ -49,7 +63,7 @@ int getRandPID(int*array) {
   srand(time(NULL));
   int r = rand() % 7;
   while(!array[r] || r == 0 || r == 1){
-    r = rand()%7;
+    r = rand() % 7;
   }
   //printf("PID: %d\n", pids[r]);
   return array[r];
@@ -69,44 +83,76 @@ int already_connected(int client_pid, int*array, int max){
 
 void subserver(int client_socket) {
   char buffer[BUFFER_SIZE];
-  int shmid = shmget(KEY, 5*sizeof(int), IPC_CREAT | 0666);
+  int shmid = shmget(KEY, 7*sizeof(int), IPC_CREAT | 0666);
   int * pids = shmat(shmid, 0, 0);
+  fd_set read_fds;
 
   while (read(client_socket, buffer, sizeof(buffer))) {
-    printf("[subserver %d] received: [%s]\n", getpid(), buffer);
+    printf("[subserver %d] received: [%s]", getpid(), buffer);
+    printf(RESET);
+    printf("\n");
+    
+    if (!pids[1]) {
+      write(client_socket, RED, BUFFER_SIZE);
+      //pids[1] = 0;
+    }
+    printf("4444 \%s \n", buffer);
 
-    //adding pid to pids array
-    if(!already_connected(getpid(),pids,7)){
-      int index=2;
-      while(pids[index] !=0 && index< 7){
-        index++;
+    fflush(stdout);
+    FD_ZERO(&read_fds);
+    FD_SET(STDIN_FILENO, &read_fds);
+    FD_SET(client_socket, & read_fds);
+    select(client_socket + 1, &read_fds, NULL, NULL, NULL);
+
+    printf("3333 \%s \n", buffer);
+    
+    if (FD_ISSET(client_socket, &read_fds)) {
+    }
+    if (FD_ISSET(STDIN_FILENO, &read_fds)) {
+      fgets(buffer, sizeof(buffer), stdin);
+      if (!strcmp(buffer, "\n")) {
+	printf("CHANGE1 pids[1]: %d\n", pids[1]);
+	pids[1] = 1;
+	printf("CHANGE2 pids[1]: %d\n", pids[1]);
+      }
+    }
+
+
+    printf("0000 \%s \n", buffer);
+    if(!already_connected(getpid(), pids, 7)){
+      int index = 2;
+      while(pids[index] != 0 && index < 7){
+	index++;
       }
       pids[index] = getpid();
       printf("pids[%d]: %d\n", index, getpid());
     }
 
-    //initial setup to turn all clients red
-    write(client_socket, RED, sizeof(char *));
-
-    //game start fxn
-    if (!pids[1] && getpid() == getRandPID(pids)) {
-      write(client_socket, CYAN, sizeof(char *));
-      pids[1] = 1;
+    printf("1111 \%s \n", buffer);
+    if (!strcmp(CYAN, buffer) && pids[1]) {
+      printf("GOT CYAN\n");
+      //pids[0] = 1;
+      write(client_socket, RED, sizeof(char *));
     }
-
-    //write(client_socket, CYAN, sizeof(char *));
-
-    //if read = blue
-    if (!strcmp(CYAN, buffer)) {
-      //getrandpid
-      pids[0] = 1;
+    printf("2222 \%s \n", buffer);
+    
+    if (pids[1] && getpid() == getRandPID(pids)) {
+      //write(client_socket, "", BUFFER_SIZE);
+      if (write(client_socket, CYAN, BUFFER_SIZE) == -1) {
+	printf("%s\n", strerror(errno));
+	exit(0);
+      }
+      printf("WRITE CYAN\n");
+      //pids[0] = 0;
     }
-    if (pids[0] && getpid() == getRandPID(pids)) {
+    /***
+    if (pids[0]) { //&& getpid() == getRandPID(pids)) {
       //if getpid = getrandpid write blue
       write(client_socket, CYAN, sizeof(char *));
       pids[0] = 0;
     }
-
+    ***/
+  
   }//end read loop
   close(client_socket);
   exit(0);
