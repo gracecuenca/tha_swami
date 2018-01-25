626abb9dd6535bdd5090817d111df451ad3d7c11
gracecuenca <gcuenca@stuy.edu>
2018-01-25 18:39:19 -0500

    hopefully can be fixed w mkfifo stuff soon

--- scroll_client.c
+++ scroll_client.c
@@ -4,19 +4,23 @@ int main(int argc, char **argv) {
 
   int server_socket;
   char buffer[BUFFER_SIZE];
-  char * color;
-  int counter = 0; //temp for now just to keep track
+  int seconds;
 
   if (argc == 2)
     server_socket = client_setup( argv[1]);
   else
     server_socket = client_setup( TEST_IP );
 
+  write(server_socket, getpid(), sizeof(getpid()));
+
   while (1) {
     write(server_socket, buffer, sizeof(buffer));
     read(server_socket, buffer, sizeof(buffer));
     clearscreen();
     char** screen = terminal_line();
-    screen_move(screen);
+    seconds = screen_move(screen);
+    if(seconds == cols()){
+      //do stuff w/ named pipes
+    }
   }
 }
--- scroll_server.c
+++ scroll_server.c
@@ -1,21 +1,60 @@
 #include "headers.h"
+#define READ 0
+#define WRITE 1
+#define MY_KEY 1001
 
 void subserver(int from_client);
+int send_text(int, char *, char *);
+//counting index in pids[]
+int index_p = 0;
 
 int main() {
 
-  int listen_socket;
+  int listen_socket, client_socket;
   int f;
+
   listen_socket = server_setup();
+  fd_set read_fds;
+  char buffer[BUFFER_SIZE]; //enter on stdin server
+  char message[BUFFER_SIZE]; //message; get ascii art
+
+  int shmid = shmget(MY_KEY, 5*sizeof(int), IPC_CREAT | 0666);
+  int* pids = shmat(shmid, 0, 0);
+
+  printf("\n\n=====Scrolling Text=====\n");
+  printf("Connect all the clients you want to send message to first, then hit ENTER\n");
 
   while (1) {
 
-    int client_socket = server_connect(listen_socket);
-    f = fork();
-    if (f == 0)
-      subserver(client_socket);
-    else
-      close(client_socket);
+    //select() modifies read_fds
+    //we must reset it at each iteration
+    FD_ZERO(&read_fds); //0 out fd set
+    FD_SET(STDIN_FILENO, &read_fds); //add stdin to fd set
+    FD_SET(listen_socket, &read_fds); //add socket to fd set
+
+    //if stdin triggered select
+    if (FD_ISSET(STDIN_FILENO, &read_fds)) {
+      fgets(buffer, sizeof(buffer), stdin);
+      //start sending text
+      if (!strcmp(buffer, "\n")) {
+        printf("Enter message:\n");
+        fgets(message, sizeof(message), stdin);
+      }
+    }
+
+    //if listen_socket triggered select
+    if (FD_ISSET(listen_socket, &read_fds) && index_p < 5) {
+      client_socket = server_connect(listen_socket);
+      f = fork();
+      if (f == 0) {
+        subserver(client_socket);
+      }
+      else {
+        close(client_socket);
+      }
+      index_p++;
+    }
+
   }
 }
 
@@ -24,8 +63,14 @@ void subserver(int client_socket) {
 
   while (read(client_socket, buffer, sizeof(buffer))) {
 
+    int shmid = shmget(MY_KEY, 5*sizeof(int), IPC_CREAT | 0666);
+    int* pids = shmat(shmid, 0, 0);
+
     printf("[subserver %d] received: [%s]\n", getpid(), buffer);
+    pids[index_p] = getpid();
+    printf("subserver %d is in %d position in array\n", pids[index_p], index_p);
     write(client_socket, buffer, sizeof(buffer));
+    
   }
 
   close(client_socket);
