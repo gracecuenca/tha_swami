7b3ae3c238a83416c6e780aad83d3c4cd5578006
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2018-01-25 23:29:53 -0500

    function

---
+++ scroll_text.c
@@ -0,0 +1,98 @@
+#include "headers.h"
+
+int rows() {
+  struct winsize w;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+  //printf("number of lines:%d\n", w.ws_row);
+  return w.ws_row;
+}
+
+int cols() {
+  struct winsize w;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+  //printf("number of cols: %d\n", w.ws_col);
+  return w.ws_col;
+}
+
+int term_size(){
+  return rows() * cols();
+}
+
+void clearscreen(){
+  printf(CLEAR);
+}
+
+void print_screen(char** screen){
+  int i;
+  for(i = 0; i < rows(); i++){
+    printf("%s\n", screen[i]);
+  }
+}
+
+char** terminal_line(char * msg){
+  int num_rows = rows();
+  int num_cols = cols();
+  char** lines = malloc(num_rows * sizeof(char*));
+  int index = 0;
+  int i = 0;
+  //where index 0 is top
+  for(index = 0; index < num_rows; index++){
+    lines[index] = malloc(num_cols * sizeof(char*));
+    while(i < num_cols - 1){
+      strcat(lines[index], " ");
+      i++;
+    }
+    i=0;
+    strcat(lines[index], &msg[0]);
+  }
+  //print_screen(lines);
+  //add freeing stuff
+  return lines;
+}
+
+char** scroll(char**init_screen, char*msg){
+  int row;
+  int col = 0;
+  int i = 0;
+  while(col < cols()){
+    for(row = 0; row < rows(); row++){
+      if(i < strlen(msg)){
+        strcat(init_screen[row], &msg[0]);
+      }
+      else{
+        strcat(init_screen[row], " ");
+      }
+    }
+    i++;
+    msg++;
+    col++;
+    clearscreen();
+    print_screen(init_screen);
+    //sleep(1);
+    usleep(50000);
+  }
+  return init_screen;
+}
+
+char** screen_shift(char** init_screen){
+  int row;
+  for(row = 0; row < rows(); row++){
+    init_screen[row]+=1;
+    strcat(init_screen[row], " ");
+  }
+  clearscreen();
+  print_screen(init_screen);
+  //add freeing stuff
+  return init_screen;
+}
+
+int screen_move(char** screen){
+  int col;
+  char** term = screen;
+  for(col = 0; col < cols(); col++){
+    term = screen_shift(term);
+    //sleep(1);
+    usleep(50000);
+  }
+  return 1;
+}
